72e7fafd99c389cfda1ca4f425cde248a6a3129f
KhanxMo <71052043+KhanxMo@users.noreply.github.com>
2021-10-23 18:33:07 -0400

    dashboard functionality

--- python.py
+++ python.py
@@ -9,25 +9,82 @@ SFWRENG 3K04
 
 
 # Importing Libraries 
+from textwrap import fill
 from tkinter import Event
 import PySimpleGUI as sg
+import random
 
 # Setting Up Global Variables 
 userCreds = [] # 2D list used for storing all registered usernames and corresponding passowords. 
+device = "0101"
+comm = 1
+pacingMode = "AOO"
+parameters = [696, 420, 7979]
 
+def editParam():
+    sg.theme('LightBrown')
+    layout = [[sg.Text('Leave blank for no change.')],
+            [sg.Text("Lower Rate Limit"), sg.InputText(key='-P0-', size=15)],
+            [sg.Text("Upper Rate Limit"), sg.InputText(key='-P1-', size=15)],
+            [sg.Text("Atrial Amplitude"), sg.InputText(key='-P2-', size=15)],
+            [sg.Button("Submit")],
+            [sg.Button('Quit')]]
+    
+    window = sg.Window('Edit Parameters', layout)
+    while True:
+        event, values = window.read()
+        if event == 'Quit' or event == sg.WIN_CLOSED:
+            window.close()
+            dashboard()
+            break
+        if event == 'Submit':
+            for i in range(0, len(parameters)):
+                keyString = "-P" + str(i) + "-"
+                if (values[keyString] != ""):
+                    parameters[i] = values[keyString]
+            window.close()
+            dashboard()
+            break
+    window.close()
+
+
+def commIndicator(key, radius=30):
+    return sg.Graph(canvas_size=(radius, radius), graph_bottom_left=(-radius, -radius), 
+    graph_top_right=(radius, radius), pad=(0, 0), key=key)
+
+def updateIndicator(window, key, color):
+    graph = window[key]
+    graph.erase()
+    graph.draw_circle((0, 0), 12, fill_color=color, line_color=color)
 
 def dashboard():
     sg.theme("LightBrown")
-    layout = [[sg.Text("Dashboard", size =(15, 1), font=40)],
+    layout = [[sg.Text("PACEMAKER Device: " + device)],
+            [sg.Text("Device Communication"), commIndicator('-Main-')],
+            [sg.Frame("Pacing Mode", [
+                [sg.Text(pacingMode, font=40)]
+            ])], 
+            [sg.Frame("Parameters", [
+                [sg.Text("Lower Rate Limit: " + str(parameters[0]))], 
+                [sg.Text("Upper Rate Limit: " + str(parameters[1]))],
+                [sg.Text("Atrial Amplitude: " + str(parameters[2]))],
+            ])], 
+            [sg.Button('Edit Paramaters')],
             [sg.Button('Quit')]]
 
     window = sg.Window("Dashboard", layout)
 
     while True:
-        event,values = window.read()
+        event,values = window.read(timeout=400)
+        
         if event == "Quit" or event == sg.WIN_CLOSED:
             break
-                
+        if event == "Edit Paramaters":
+            window.close()
+            editParam()
+            break
+    
+        updateIndicator(window, '-Main-', 'red' if comm==0 else 'green')
 
     window.close()
 
@@ -50,8 +107,8 @@ def progress_bar():
 def create_account():
     sg.theme('LightBrown')
     layout = [[sg.Text("Sign Up", size =(15, 1), font=40, justification='c')],
-             [sg.Text("Create Username", size =(15, 1), font=16), sg.InputText(key='-username-', font=16)],
-             [sg.Text("Create Password", size =(15, 1), font=16), sg.InputText(key='-password-', font=16, password_char='*')],
+             [sg.Text("Create Username", size =(15, 1), font=16), sg.InputText(key='-username-', font=16, size=15)],
+             [sg.Text("Create Password", size =(15, 1), font=16), sg.InputText(key='-password-', font=16, password_char='*', size=15)],
              [sg.Button("Submit"), sg.Button("Cancel")]]
  
     window = sg.Window("Sign Up", layout)
@@ -82,14 +139,12 @@ def create_account():
                 
     window.close()
 
-
-
 def login():
     sg.theme("LightBrown")
-    layout = [[sg.Text("Log In", size =(15, 1), font=40)],
-            [sg.Text("Username", size =(15, 1), font=16),sg.InputText(key='-usrnm-', font=16)],
-            [sg.Text("Password", size =(15, 1), font=16),sg.InputText(key='-pwd-', password_char='*', font=16)],
-            [sg.Button('Login'),sg.Button('Quit'), sg.Button('Register')]]
+    layout = [[sg.Text("Log In", size =(10, 1), font=40)],
+            [sg.Text("Username", size =(10, 1), font=16),sg.InputText(key='-usrnm-', font=16, size=15)],
+            [sg.Text("Password", size =(10, 1), font=16),sg.InputText(key='-pwd-', password_char='*', font=16, size=15)],
+            [sg.Button('Login'),sg.Button('Register'), sg.Button('Quit')]]
 
     window = sg.Window("Log In", layout)
 
@@ -145,8 +200,7 @@ def checkCreds(name, passwor):
     for line in file1.readlines():
         userCreds.append(line.split())
     file1.close
-    print(userCreds)
-
+    
     for cred in userCreds:
         if (cred[0] == name) & (cred[1] == passwor):
             return 1
